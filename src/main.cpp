bd47222d6d293e9ab72dc37cfedb134a99fbe909
Daisuke Bauza <144384874+daisuke1227@users.noreply.github.com>
2025-04-17 10:17:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,64 +35,83 @@ static const auto IMPORT_PICK_OPTIONS = file::FilePickOptions{
     {{"GD Level Files", {"*.gmd", "*.gmdl"}}}
 };
 
+// Helper to get app writable directory + filename
+static std::filesystem::path getOriginalPath(const std::string& name) {
+#if GEODE_IOS || GEODE_ANDROID
+    std::string dir = cocos2d::FileUtils::getInstance()->getWritablePath();
+#else
+    auto dir = std::filesystem::current_path().string();
+#endif
+    return std::filesystem::path(dir) / name;
+}
+
 template <class L>
 static Task<Result<std::filesystem::path>> promptExportLevel(L* level) {
     auto opts = IMPORT_PICK_OPTIONS;
-    if constexpr (std::is_same_v<L, GJLevelList>) {
-        opts.defaultPath = std::filesystem::path(std::string(level->m_listName) + ".gmdl");
-    } else {
-        opts.defaultPath = std::filesystem::path(std::string(level->m_levelName) + ".gmd");
-    }
+    std::string base = std::is_same_v<L, GJLevelList>
+        ? std::string(level->m_listName) + ".gmdl"
+        : std::string(level->m_levelName) + ".gmd";
+    opts.defaultPath = base;
     return file::pick(file::PickMode::SaveFile, opts);
 }
 
 template <class L>
 static void onExportFilePick(L* level, typename Task<Result<std::filesystem::path>>::Event* event) {
     if (auto result = event->getValue(); result && result->isOk()) {
-        auto path = result->unwrap();
+        auto userPath = result->unwrap();
+        std::string filename = userPath.filename().string();
+        auto originalPath = getOriginalPath(filename);
         std::optional<std::string> err;
-        if constexpr (std::is_same_v<L, GJLevelList>) {
-            err = exportListAsGmd(level, path).err();
-        } else {
-            err = exportLevelAsGmd(level, path).err();
-        }
+        if constexpr (std::is_same_v<L, GJLevelList>)
+            err = exportListAsGmd(level, originalPath).err();
+        else
+            err = exportLevelAsGmd(level, originalPath).err();
         if (err) {
-            FLAlertLayer::create("Error", "Unable to export: " + *err, "OK")->show();
+            FLAlertLayer::create("Error", "Unable to export original: " + *err, "OK")->show();
             return;
         }
-
+        // copy to user selected folder
+        try {
+            std::filesystem::copy_file(
+                originalPath,
+                userPath,
+                std::filesystem::copy_options::overwrite_existing
+            );
+        } catch (std::exception& e) {
+            FLAlertLayer::create("Error", "Copy failed: " + std::string(e.what()), "OK")->show();
+            return;
+        }
 #if GEODE_IOS
         auto director = CCDirector::sharedDirector();
         auto view = director->getOpenGLView();
         auto uiView = view->getEAGLView();
-        auto nsPath = __String::create(path.string().c_str())->getCString();
-        NSURL* fileURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:nsPath]];
+        auto ns = __String::create(userPath.string().c_str())->getCString();
+        NSURL* fileURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:ns]];
         UIDocumentInteractionController* ctrl =
             [UIDocumentInteractionController interactionControllerWithURL:fileURL];
         ctrl.delegate = (id<UIDocumentInteractionControllerDelegate>)view;
-        CGRect frame = CGRectMake(0, 0, uiView.bounds.size.width, uiView.bounds.size.height);
+        CGRect frame = CGRectMake(0,0, uiView.bounds.size.width, uiView.bounds.size.height);
         [ctrl presentOptionsMenuFromRect:frame inView:uiView animated:YES];
 #elif GEODE_ANDROID
-        {
-            cocos2d::JniMethodInfo methodInfo;
-            if (cocos2d::JniHelper::getStaticMethodInfo(methodInfo,
-                "org/cocos2dx/lib/Cocos2dxActivity",
-                "shareGmdFile",
-                "(Ljava/lang/String;)V"))
-            {
-                jstring jPath = methodInfo.env->NewStringUTF(path.string().c_str());
-                methodInfo.env->CallStaticVoidMethod(methodInfo.classID, methodInfo.methodID, jPath);
-                methodInfo.env->DeleteLocalRef(jPath);
-                methodInfo.env->DeleteLocalRef(methodInfo.classID);
-            }
+        cocos2d::JniMethodInfo info;
+        if (cocos2d::JniHelper::getStaticMethodInfo(
+            info,
+            "org/cocos2dx/lib/Cocos2dxActivity",
+            "shareGmdFile",
+            "(Ljava/lang/String;)V"
+        )) {
+            jstring jPath = info.env->NewStringUTF(userPath.string().c_str());
+            info.env->CallStaticVoidMethod(info.classID, info.methodID, jPath);
+            info.env->DeleteLocalRef(jPath);
+            info.env->DeleteLocalRef(info.classID);
         }
 #else
         createQuickPopup(
             "Exported",
-            "Successfully exported",
+            "Saved to: " + userPath.string(),
             "OK", "Open Folder",
-            [path](auto, bool openIt) {
-                if (openIt) file::openFolder(path);
+            [userPath](auto, bool openIt) {
+                if (openIt) file::openFolder(userPath);
             }
         );
 #endif
@@ -103,129 +122,87 @@ static void onExportFilePick(L* level, typename Task<Result<std::filesystem::pat
 
 struct $modify(ExportMyLevelLayer, EditLevelLayer) {
     struct Fields { EventListener<Task<Result<std::filesystem::path>>> pickListener; };
-
     $override
     bool init(GJGameLevel* level) {
         if (!EditLevelLayer::init(level)) return false;
-        if (auto menu = this->getChildByID("level-actions-menu")) {
+        if (auto m = getChildByID("level-actions-menu")) {
             auto btn = CCMenuItemSpriteExtra::create(
-                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr, .8f,
-                    CircleBaseColor::Green, CircleBaseSize::MediumAlt),
-                this, menu_selector(ExportMyLevelLayer::onExport)
-            );
-            btn->setID("export-button"_spr);
-            menu->addChild(btn);
-            menu->updateLayout();
+                CircleButtonSprite::createWithSpriteFrameName(
+                    "file.png"_spr, .8f,
+                    CircleBaseColor::Green, CircleBaseSize::MediumAlt
+                ), this, menu_selector(ExportMyLevelLayer::onExport)
+            ); btn->setID("export-button"_spr);
+            m->addChild(btn);
+            m->updateLayout();
         }
         return true;
     }
-
     void onExport(CCObject*) {
-        m_fields->pickListener.bind([level = m_level](auto* ev) { onExportFilePick(level, ev); });
+        m_fields->pickListener.bind([lvl=m_level](auto* e){ onExportFilePick(lvl,e); });
         m_fields->pickListener.setFilter(promptExportLevel(m_level));
     }
 };
 
 struct $modify(ExportOnlineLevelLayer, LevelInfoLayer) {
     struct Fields { EventListener<Task<Result<std::filesystem::path>>> pickListener; };
-
     $override
-    bool init(GJGameLevel* level, bool challenge) {
-        if (!LevelInfoLayer::init(level, challenge)) return false;
-        if (auto menu = this->getChildByID("left-side-menu")) {
-            auto btn = CCMenuItemSpriteExtra::create(
-                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr, .8f,
-                    CircleBaseColor::Green, CircleBaseSize::Medium),
-                this, menu_selector(ExportOnlineLevelLayer::onExport)
-            );
-            btn->setID("export-button"_spr);
-            menu->addChild(btn);
-            menu->updateLayout();
-        }
-        return true;
+    bool init(GJGameLevel* level,bool c){ if(!LevelInfoLayer::init(level,c))return false;
+        if(auto m=getChildByID("left-side-menu")){
+            auto btn=CCMenuItemSpriteExtra::create(
+                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr,.8f,
+                    CircleBaseColor::Green,CircleBaseSize::Medium),
+                this,menu_selector(ExportOnlineLevelLayer::onExport)
+            );btn->setID("export-button"_spr);
+            m->addChild(btn);m->updateLayout();
+        }return true;
     }
-
-    void onExport(CCObject*) {
-        m_fields->pickListener.bind([level = m_level](auto* ev) { onExportFilePick(level, ev); });
+    void onExport(CCObject*){
+        m_fields->pickListener.bind([lvl=m_level](auto*e){onExportFilePick(lvl,e);});
         m_fields->pickListener.setFilter(promptExportLevel(m_level));
     }
 };
 
 struct $modify(ImportLayer, LevelBrowserLayer) {
     struct Fields { EventListener<Task<Result<std::vector<std::filesystem::path>>>> pickListener; };
-
-    static void importFiles(const std::vector<std::filesystem::path>& paths) {
-        for (auto const& path : paths) {
-            switch (getGmdFileKind(path)) {
-                case GmdFileKind::List: {
-                    auto res = importGmdAsList(path);
-                    if (res) LocalLevelManager::get()->m_localLists->insertObject(*res, 0);
-                    else { FLAlertLayer::create("Error Importing", res.unwrapErr(), "OK")->show(); return; }
-                } break;
-                case GmdFileKind::Level: {
-                    auto res = importGmdAsLevel(path);
-                    if (res) LocalLevelManager::get()->m_localLevels->insertObject(*res, 0);
-                    else { FLAlertLayer::create("Error Importing", res.unwrapErr(), "OK")->show(); return; }
-                } break;
-                case GmdFileKind::None:
-                    FLAlertLayer::create("Error Importing", fmt::format("Selected file '<cp>{}</c>' is not a GMD file!", path.string()), "OK")->show();
-                    return;
-            }
-        }
-        auto scene = CCScene::create();
-        auto layer = LevelBrowserLayer::create(GJSearchObject::create(SearchType::MyLevels));
-        scene->addChild(layer);
-        CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(.5f, scene));
+    static void importFiles(const std::vector<std::filesystem::path>& p){
+        for(auto& f:p){switch(getGmdFileKind(f)){
+            case GmdFileKind::List:{auto r=importGmdAsList(f);if(r)LocalLevelManager::get()->m_localLists->insertObject(*r,0);
+                else{FLAlertLayer::create("Error Importing",r.unwrapErr(),"OK")->show();return;}}break;
+            case GmdFileKind::Level:{auto r=importGmdAsLevel(f);if(r)LocalLevelManager::get()->m_localLevels->insertObject(*r,0);
+                else{FLAlertLayer::create("Error Importing",r.unwrapErr(),"OK")->show();return;}}break;
+            case GmdFileKind::None:
+                FLAlertLayer::create("Error Importing",fmt::format("Selected '<cp>{}</c>' not GMD!",f.string()),"OK")->show();return;
+        }}auto sc=CCScene::create();auto ly=LevelBrowserLayer::create(GJSearchObject::create(SearchType::MyLevels));sc->addChild(ly);
+        CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(.5f,sc));
     }
-
-    void onImport(CCObject*) {
-        m_fields->pickListener.bind([](auto* ev) {
-            if (auto result = ev->getValue(); result && result->isOk()) importFiles(result->unwrap());
-            else if (result) FLAlertLayer::create("Error Importing", result->unwrapErr(), "OK")->show();
-        });
+    void onImport(CCObject*){
+        m_fields->pickListener.bind([](auto*e){if(auto r=e->getValue();r&&r->isOk())importFiles(r->unwrap());else if(r)FLAlertLayer::create("Error Importing",r->unwrapErr(),"OK")->show();});
         m_fields->pickListener.setFilter(file::pickMany(IMPORT_PICK_OPTIONS));
     }
-
     $override
-    bool init(GJSearchObject* search) {
-        if (!LevelBrowserLayer::init(search)) return false;
-        if (search->m_searchType == SearchType::MyLevels || search->m_searchType == SearchType::MyLists) {
-            auto btnMenu = this->getChildByID("new-level-menu");
-            auto importBtn = CCMenuItemSpriteExtra::create(
-                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr, .85f,
-                    CircleBaseColor::Pink, CircleBaseSize::Big),
-                this, menu_selector(ImportLayer::onImport)
-            );
-            importBtn->setID("import-level-button"_spr);
-            if (search->m_searchType == SearchType::MyLists && search->m_searchIsOverlay)
-                btnMenu->addChildAtPosition(importBtn, Anchor::BottomLeft, ccp(0, 60), false);
-            else { btnMenu->addChild(importBtn); btnMenu->updateLayout(); }
-        }
-        return true;
+    bool init(GJSearchObject*s){if(!LevelBrowserLayer::init(s))return false;
+        if(s->m_searchType==SearchType::MyLevels||s->m_searchType==SearchType::MyLists){auto m=getChildByID("new-level-menu");auto ib=CCMenuItemSpriteExtra::create(
+                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr,.85f,CircleBaseColor::Pink,CircleBaseSize::Big),
+                this,menu_selector(ImportLayer::onImport)
+            );ib->setID("import-level-button"_spr);
+            if(s->m_searchType==SearchType::MyLists&&s->m_searchIsOverlay) m->addChildAtPosition(ib,Anchor::BottomLeft,ccp(0,60),false);
+            else{m->addChild(ib);m->updateLayout();}
+        }return true;
     }
 };
 
 struct $modify(ExportListLayer, LevelListLayer) {
     struct Fields { EventListener<Task<Result<std::filesystem::path>>> pickListener; };
-
     $override
-    bool init(GJLevelList* level) {
-        if (!LevelListLayer::init(level)) return false;
-        if (auto menu = this->getChildByID("left-side-menu")) {
-            auto btn = CCMenuItemSpriteExtra::create(
-                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr, .8f,
-                    CircleBaseColor::Green, CircleBaseSize::Medium),
-                this, menu_selector(ExportListLayer::onExport)
-            );
-            btn->setID("export-button"_spr);
-            menu->addChild(btn);
-            menu->updateLayout();
-        }
+    bool init(GJLevelList* l){if(!LevelListLayer::init(l))return false;
+        if(auto m=getChildByID("left-side-menu")){auto b=CCMenuItemSpriteExtra::create(
+                CircleButtonSprite::createWithSpriteFrameName("file.png"_spr,.8f,CircleBaseColor::Green,CircleBaseSize::Medium),
+                this,menu_selector(ExportListLayer::onExport)
+            );b->setID("export-button"_spr);m->addChild(b);m->updateLayout();}
         return true;
     }
-
-    void onExport(CCObject*) {
-        m_fields->pickListener.bind([list = m_levelList](auto* ev) { onExportFilePick(list, ev); });
+    void onExport(CCObject*){
+        m_fields->pickListener.bind([lst=m_levelList](auto*e){onExportFilePick(lst,e);});
         m_fields->pickListener.setFilter(promptExportLevel(m_levelList));
     }
 };
